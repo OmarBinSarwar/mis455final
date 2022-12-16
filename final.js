88fc7f41e0d7902ea14fccc1a00650ecf91fa075
OmarBinSarwar <115527501+OmarBinSarwar@users.noreply.github.com>
2022-12-16 14:04:51 +0600

    Add files via upload

--- final.js
+++ final.js
@@ -1,34 +1,33 @@
-let meal = [];
 function search() {
-    fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    
+    let searchInput = document.getElementById('searchfood').value;
+
+    let url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchInput}`;
+
+    fetch(url)
     .then((response) => response.json())
     .then((data) => {
-        meal = data.categories;
+        display(data.meals);
     });
-
-    let searchInput = document.getElementById('searchfood').value;
+}
+function display(meal) {
+    
     let searchNumber= document.getElementById('searchnumber').value;
     let count=0;
     let searchResult = document.getElementById('card-deck');
     let searchResultHTML = '';
     meal.forEach((meal) => {
-        if (meal.strCategory.toLowerCase().includes(searchInput.toLowerCase()) && count<searchNumber) {
+        if(count<searchNumber){
             searchResultHTML += `
-            <div class="col-md-12">
-                <div class="card mb-4 shadow-sm">
-                    <img src="${meal.strCategoryThumb}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h1 class="card-title">${meal.strCategory}</h1>
-                        <p class="card-text">${meal.strCategoryDescription}</p>
-                    </div>
+            <div class="card">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h1 class="card-title">${meal.strMeal}</h1>
                 </div>
             </div>
             `;
-
-
             count++;
         }
     });
     searchResult.innerHTML = searchResultHTML;
-
 }
